bb08c55a7db2842b2a283d73003b253f9f5938b4
Peter <Posoo617@gmail.com>
2020-05-23 19:57:21 +0300

    Removing the local state, Single Source of Truth

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,23 +1,15 @@
 import React, { Component } from "react"
 
 class Counters extends Component {
-	state = {
-		value: this.props.value,
-	}
-
 	formatCount() {
-		const { value } = this.state
+		const { value } = this.props.counter
 		return value === 0 ? "Zero" : value
 	}
 
 	getBadgeClasses() {
 		let classes = "badge m-2 badge-"
-		classes += this.state.value === 0 ? "warning" : "primary"
+		classes += this.props.counter.value === 0 ? "warning" : "primary"
 		return classes
-	}
-
-	handleIncrement = () => {
-		this.setState({ value: this.state.value + 1 })
 	}
 
 	render() {
@@ -25,10 +17,16 @@ class Counters extends Component {
 			<div>
 				<span className={this.getBadgeClasses()}> {this.formatCount()} </span>
 				<button
-					onClick={this.handleIncrement}
+					onClick={() => this.props.onIncrement(this.props.counter)}
 					className="btn btn-secondary btn-sm"
 				>
 					Increment
+				</button>
+				<button
+					onClick={() => this.props.onDelete(this.props.counter.id)}
+					className="btn btn-danger btn-sm m-2"
+				>
+					Delete
 				</button>
 			</div>
 		)
