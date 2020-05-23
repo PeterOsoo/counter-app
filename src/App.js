65ba9fdbac0e55395dc70407e002962c7c080484
Peter <Posoo617@gmail.com>
2020-05-23 10:27:17 +0300

    Passing Event Arguments, Composing Components & Passing Data to Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
 import Counters from "./components/Counters"
 
--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -1,26 +1,21 @@
 import React, { Component } from "react"
+import Counter from "./Counter"
 
 class Counters extends Component {
 	state = {
-		count: 0,
+		counters: [
+			{ id: 1, value: 0 },
+			{ id: 2, value: 0 },
+			{ id: 3, value: 5 },
+			{ id: 4, value: 0 },
+		],
 	}
-
-	formatCount() {
-		const { count } = this.state
-		return count === 0 ? "Zero" : count
-	}
-
-	getBadgeClasses() {
-		let classes = "badge m-2 badge-"
-		classes += this.state.count === 0 ? "warning" : "primary"
-		return classes
-	}
-
 	render() {
 		return (
 			<div>
-				<span className={this.getBadgeClasses()}> {this.formatCount()} </span>
-				<button className="btn btn-secondary btn-sm"> Increment</button>
+				{this.state.counters.map(counter => (
+					<Counter key={counter.id} value={counter.value} />
+				))}
 			</div>
 		)
 	}
